f99f6b488a69f75ca29b22b1584fd16a97880c35
4aykovski <kan89kan@gmail.com>
2024-11-06 18:41:08 +0300

    move connector app and add chan to stop it
    move connector app from main to separate files
    add chan struct{} to gracefully stop connector app and start another app

--- backend/cmd/app/main.go
+++ backend/cmd/app/main.go
@@ -1,113 +1,31 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"net/http"
-	"os/exec"
-	"strings"
+
+	connectorApp "github.com/4aykovski/iot-hub/backend/internal/connector/app"
 )
 
 func main() {
-	http.HandleFunc("GET /", ChooseConnect)
-	http.HandleFunc("POST /connect", TryToConnect)
+	done := make(chan struct{})
 
-	http.ListenAndServe(":18080", nil)
-}
-
-func ChooseConnect(w http.ResponseWriter, r *http.Request) {
-	app := "/bin/bash"
-	cmd := exec.CommandContext(
-		r.Context(),
-		app,
-		"-c",
-		"nmcli --colors no -m multiline --get-value SSID dev wifi list",
-	)
-
-	out, err := cmd.Output()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	res := `
-  <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Wifi Control</title>
-        </head>
-        <body>
-            <h1>Wifi Control</h1>
-            <form action="/connect" method="post">
-                <label for="ssid">Choose a WiFi network:</label>
-                <select name="ssid" id="ssid">
-  `
-
-	ssids := strings.Replace(string(out), "SSID:", "", -1)
-	ssidsList := strings.Split(ssids, "\n")
-
-	for _, ssid := range ssidsList {
-		if ssid != "" {
-			res += fmt.Sprintf("<option value=\"%s\">%s</option>", ssid, ssid)
+	connector := connectorApp.New(done)
+	go func() {
+		if err := connector.Start(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				return
+			}
+			panic(err)
 		}
+	}()
+
+	<-done
+	if err := connector.GracefullStop(context.Background()); err != nil {
+		panic(err)
 	}
 
-	res += `
-                </select>
-                <p/>
-                <label for="password">Password: <input type="password" name="password"/></label>
-                <p/>
-                <input type="submit" value="Connect">
-            </form>
-        </body>
-        </html>
-  `
-
-	fmt.Fprintf(w, res)
-}
-
-func TryToConnect(w http.ResponseWriter, r *http.Request) {
-	ssid := r.FormValue("ssid")
-	password := r.FormValue("password")
-
-	app := "/bin/bash"
-	cmd := exec.CommandContext(
-		r.Context(),
-		app,
-		"-c",
-		"nmcli dev wifi connect "+ssid,
-	)
-
-	if len(password) > 0 {
-		cmd.Args[2] += " password " + password
-	}
-
-	_, err := cmd.Output()
-	if err != nil {
-		var exitErr *exec.ExitError
-		if errors.As(err, &exitErr) {
-			fmt.Println(string(exitErr.Stderr))
-			fmt.Fprintf(w, "Error: %s", string(exitErr.Stderr))
-			return
-		}
-
-		fmt.Println(err.Error())
-		fmt.Fprintf(w, "Error: %s", err.Error())
-		return
-	}
-
-	res := `
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Wifi Control</title>
-        </head>
-        <body>
-  `
-	res += fmt.Sprintf("Successfully connected to %s", ssid)
-	res += `
-        </body>
-        </html>
-  `
-
-	fmt.Fprintf(w, res)
+	fmt.Println("done")
 }
---
+++ backend/internal/connector/transport/http/v1/mux.go
@@ -0,0 +1,174 @@
+package v1
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os/exec"
+	"strings"
+)
+
+func New(done chan struct{}) *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", ChooseConnect)
+	mux.HandleFunc("/connect", TryToConnect(done))
+
+	return mux
+}
+
+func ChooseConnect(w http.ResponseWriter, r *http.Request) {
+	slog.Info("get connector request")
+
+	ssidsList := getWifiList(r.Context())
+
+	res := `
+  <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Wifi Control</title>
+        </head>
+        <body>
+            <h1>Wifi Control</h1>
+            <form action="/connect" method="post">
+                <label for="ssid">Choose a WiFi network:</label>
+                <select name="ssid" id="ssid">
+  `
+
+	for _, ssid := range ssidsList {
+		if ssid != "" {
+			res += fmt.Sprintf("<option value=\"%s\">%s</option>", ssid, ssid)
+		}
+	}
+
+	res += `
+                </select>
+                <p/>
+                <label for="password">Password: <input type="password" name="password"/></label>
+                <p/>
+                <input type="submit" value="Connect">
+            </form>
+        </body>
+        </html>
+  `
+
+	slog.Info("return html with wifi list")
+
+	fmt.Fprintf(w, res)
+}
+
+func TryToConnect(done chan struct{}) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("post connector request")
+
+		ssid := r.FormValue("ssid")
+		password := r.FormValue("password")
+
+		app := "/bin/bash"
+
+		if ifWasConnected(r.Context(), ssid) {
+			dropConnection := exec.CommandContext(
+				r.Context(),
+				app,
+				"-c",
+				"nmcli connection delete "+ssid,
+			)
+
+			_, err := dropConnection.Output()
+			if err != nil {
+				var exitErr *exec.ExitError
+				if errors.As(err, &exitErr) {
+					fmt.Println(string(exitErr.Stderr))
+					fmt.Fprintf(w, "Error: %s", string(exitErr.Stderr))
+					return
+				}
+
+				fmt.Println(err.Error())
+				fmt.Fprintf(w, "Error: %s", err.Error())
+				return
+			}
+		}
+
+		cmd := exec.CommandContext(
+			r.Context(),
+			app,
+			"-c",
+			"nmcli dev wifi connect "+ssid,
+		)
+
+		if len(password) > 0 {
+			cmd.Args[2] += " password " + password
+		}
+
+		_, err := cmd.Output()
+		if err != nil {
+			var exitErr *exec.ExitError
+			if errors.As(err, &exitErr) {
+				fmt.Println(string(exitErr.Stderr))
+				fmt.Fprintf(w, "Error: %s", string(exitErr.Stderr))
+				return
+			}
+
+			fmt.Println(err.Error())
+			fmt.Fprintf(w, "Error: %s", err.Error())
+			return
+		}
+
+		res := `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Wifi Control</title>
+        </head>
+        <body>
+  `
+		res += fmt.Sprintf("Successfully connected to %s", ssid)
+		res += `
+        </body>
+        </html>
+  `
+
+		slog.Info("successfully connected to wifi")
+
+		fmt.Fprintf(w, res)
+
+		done <- struct{}{}
+	}
+}
+
+func ifWasConnected(ctx context.Context, ssid string) bool {
+	app := "/bin/bash"
+	cmd := exec.CommandContext(ctx, app, "-c", "nmcli connection | grep "+ssid)
+	out, err := cmd.Output()
+	if err != nil {
+		return false
+	}
+
+	if len(out) <= 0 {
+		return false
+	}
+
+	return true
+}
+
+func getWifiList(ctx context.Context) []string {
+	app := "/bin/bash"
+	cmd := exec.CommandContext(
+		ctx,
+		app,
+		"-c",
+		"nmcli --colors no -m multiline --get-value SSID dev wifi list",
+	)
+
+	out, err := cmd.Output()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ssids := strings.Replace(string(out), "SSID:", "", -1)
+	ssidsList := strings.Split(ssids, "\n")
+
+	return ssidsList
+}

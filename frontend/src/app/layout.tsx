18fcd764cc4179076857b5898d0d28f9ddf45b89
4aykovski <kan89kan@gmail.com>
2024-11-19 15:28:24 +0700

    feat: header

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import { ModeToggle } from "@/components/ModeToggle";
+import { ThemeProvider } from "./ThemeProvider";
+import "./globals.css";
+import { Navigation } from "./Navigation";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning={true}>
+      <head></head>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="flex px-10 py-5 justify-between">
+            <Navigation />
+            <ModeToggle />
+          </header>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}

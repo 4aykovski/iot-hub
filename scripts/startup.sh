5a6fd0ff67137c60854c330d36b456760c6dc97f
4aykovski <kan89kan@gmail.com>
2024-11-27 12:53:25 +0000

    fixes

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,17 +1,27 @@
+#!/bin/bash
+
 BACKEND_SERVER_PATH="/home/chaykovski/apps/iot-hub/backend"
 FRONTEND_SERVER_PATH="/home/chaykovski/apps/iot-hub/frontend"
 
 export CGO_ENABLED=0
 
+echo "starting net app"
+
 cd $BACKEND_SERVER_PATH
 go run ./cmd/net/main.go \
   -subnet 0.0.0 -port 19050 -path /data -output $BACKEND_SERVER_PATH/configs/.env.device 
 
+echo "starting connector app"
+
 cd $BACKEND_SERVER_PATH
 go run ./cmd/connector/main.go
 
+echo "starting frontend"
+
 cd $FRONTEND_SERVER_PATH
 bun --bun run dev &
+
+echo "starting backend"
 
 cd $BACKEND_SERVER_PATH
 go run ./cmd/iot/main.go

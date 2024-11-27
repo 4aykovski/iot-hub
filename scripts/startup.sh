d5d580390845a1551da55a0535c1991b14d316f9
4aykovski <kan89kan@gmail.com>
2024-11-27 14:02:49 +0000

    service

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 BACKEND_SERVER_PATH="/home/chaykovski/apps/iot-hub/backend"
 FRONTEND_SERVER_PATH="/home/chaykovski/apps/iot-hub/frontend"
 
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export GOROOT=/usr/local/go
 export CGO_ENABLED=0
 
 echo "starting net app"
+
+go version
 
 cd $BACKEND_SERVER_PATH
 go run ./cmd/net/main.go \
